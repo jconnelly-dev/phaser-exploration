ef27814318408f48ed0f44fc35e6ae949289333d
joseph connelly <jconnelly@precisemrm.com>
2020-05-09 16:33:19 -0600

    total shift - removed all his images and replace w/my own :)

--- game/Game.js
+++ game/Game.js
@@ -2,7 +2,14 @@ GameBoard.Game = function(game) {};
 
 GameBoard.Game.prototype = {
     
-    create: function() {},
+    create: function() {
+        this.buildWorld();
+    },
+    
+    buildWorld: function() {
+        this.add.image(0, 0, 'landscape');
+        this.add.image(35, 367, 'character');
+    },
     
     update: function() {}
 }
